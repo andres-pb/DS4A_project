9fb290726b36649fd29408fbf5531061bb59c266
Prisspr888 <priscillapalacios04@gmail.com>
2022-06-07 15:43:28 -0500

    included symbol in the twt_query column

--- app/api/coin_marketcap.py
+++ app/api/coin_marketcap.py
@@ -20,11 +20,13 @@ class CoinMarketCap:
             )
         topcoins_df.sort_values(by='rank', inplace=True)
         topcoins_df = topcoins_df.query('is_active==1')
-        topcoins_df = topcoins_df.iloc[:top_n, :]
+        topcoins_df = topcoins_df.iloc[:top_n, :].reset_index()
 
         # Add queries for twitter api
+        topcoins_df['hash_symbol'] = '#' + topcoins_df['symbol']
         topcoins_df['hash_slug'] = '#' + topcoins_df['slug']
-        topcoins_df['twt_query'] = topcoins_df[['name', 'slug', 'hash_slug']].agg(' OR '.join, axis=1)
+        keyword_cols = ['symbol', 'hash_symbol', 'name', 'slug', 'hash_slug']
+        topcoins_df['twt_query'] = topcoins_df[keyword_cols].agg(' OR '.join, axis=1)
         return topcoins_df
 
 
@@ -32,5 +34,7 @@ class CoinMarketCap:
 load_dotenv()
 mktcap_key = environ.get('COIN_MKTCAP_KEY')
 cmc = CoinMarketCap(mktcap_key)
-cmc.get_top_coins().to_csv('presel_coins.csv', index=False)
-print(cmc.get_top_coins(10))
+coins_df = cmc.get_top_coins()
+# Save top 50 coins metadata
+coins_df.to_csv('presel_coins.csv', index=False)
+print(coins_df['twt_query'])
