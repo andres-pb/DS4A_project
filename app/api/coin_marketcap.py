609fb589c66660eb4df76f39e3b9dcb656a18d98
Prisspr888 <priscillapalacios04@gmail.com>
2022-06-07 19:31:34 -0500

    required coins with enough days of history to ease model training

--- app/api/coin_marketcap.py
+++ app/api/coin_marketcap.py
@@ -2,6 +2,7 @@ import coinmarketcapapi
 import pandas as pd
 from os import environ
 from dotenv import load_dotenv
+import datetime as dt
 
 
 class CoinMarketCap:
@@ -11,14 +12,21 @@ class CoinMarketCap:
         self.api = coinmarketcapapi.CoinMarketCapAPI(self.key)
 
 
-    def get_top_coins(self, top_n=50):
+    def get_top_coins(self, top_n:int=50, sort_col:str='rank', sort_asc:bool=True, min_days:int=2400):
 
         data_id_map = self.api.cryptocurrency_map().data
-        topcoins_df = pd.DataFrame.from_records(data_id_map).drop(columns=[
-                'platform', 'last_historical_data', 'first_historical_data'
-                ]
-            )
-        topcoins_df.sort_values(by='rank', inplace=True)
+        topcoins_df = pd.DataFrame.from_records(data_id_map)
+
+        # Require a minimum of history days
+        topcoins_df['first_historical_data'] = pd.to_datetime(topcoins_df['first_historical_data'])
+        topcoins_df['last_historical_data'] = pd.to_datetime(topcoins_df['last_historical_data'])
+        topcoins_df['days_history'] = topcoins_df['last_historical_data'] - topcoins_df['first_historical_data']
+        topcoins_df.drop(columns=['platform', 'last_historical_data', 'first_historical_data'], inplace=True)
+        min_hist = dt.timedelta(days=min_days)
+        topcoins_df = topcoins_df[topcoins_df['days_history'] >= min_hist]
+
+        # Sort by importance
+        topcoins_df.sort_values(by=sort_col, ascending=sort_asc, inplace=True)
         topcoins_df = topcoins_df.query('is_active==1')
         topcoins_df = topcoins_df.iloc[:top_n, :].reset_index()
 
@@ -27,6 +35,7 @@ class CoinMarketCap:
         topcoins_df['hash_slug'] = '#' + topcoins_df['slug']
         keyword_cols = ['symbol', 'hash_symbol', 'name', 'slug', 'hash_slug']
         topcoins_df['twt_query'] = topcoins_df[keyword_cols].agg(' OR '.join, axis=1)
+
         return topcoins_df
 
 
@@ -37,4 +46,4 @@ cmc = CoinMarketCap(mktcap_key)
 coins_df = cmc.get_top_coins()
 # Save top 50 coins metadata
 coins_df.to_csv('presel_coins.csv', index=False)
-print(coins_df['twt_query'])
+print(coins_df)
