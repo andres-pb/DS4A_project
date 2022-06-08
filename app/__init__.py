8ae7ea84c4fd3e4b6462b3a1edf5c4f69dd77365
Prisspr888 <priscillapalacios04@gmail.com>
2022-06-08 01:41:13 -0500

    import GoogleTrends

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,2 @@
-from .api import Polygon, Alphavantage, yahoo_finance, Twitter
+from .api import Polygon, Alphavantage, yahoo_finance, Twitter, GoogleTrends, CoinMarketCap
 from .modules import Statistical
