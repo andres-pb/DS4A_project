32730b4f6e778600d87c42d4dd97cf90220c5d61
andres-pb <andres-pb10@hotmail.com>
2022-06-27 01:08:04 -0500

    FinBERT

--- sentiment_models/finbert/download_code.py
+++ sentiment_models/finbert/download_code.py
@@ -5,5 +5,5 @@ from transformers import pipeline
     #FinBERT
 finbert_tokenizer = BertTokenizer.from_pretrained("ProsusAI/finbert")                                                               #Download
 finbert_model = BertForSequenceClassification.from_pretrained("ProsusAI/finbert", num_labels=3)                                     #Download
-finbert_model.save_pretrained("./sentiment_models/finbert_model")                                                                         #Model is saved for local use
-finbert_tokenizer.save_pretrained("./sentiment_models/finbert_tokenizer")                                                           #Model tokeneizer is saved for local use
+finbert_model.save_pretrained("./sentiment_models/finbert/finbert_model")                                                     #Model is saved for local use
+finbert_tokenizer.save_pretrained("./sentiment_models/finbert/finbert_tokenizer")                                              #Model tokeneizer is saved for local use
