79bf6f9c86c917a0dc66a3eaf4457dd7fd5df2a9
andres-pb <andres-pb10@hotmail.com>
2022-06-27 00:40:06 -0500

    Model download py

--- sentiment_models/financial_bert/download_code.py
+++ sentiment_models/financial_bert/download_code.py
@@ -5,11 +5,7 @@ from transformers import pipeline
     #FinancialBERT
 financialbert_tokenizer = BertTokenizer.from_pretrained("ahmedrachid/FinancialBERT-Sentiment-Analysis")                             #Download
 financialbert_model = BertForSequenceClassification.from_pretrained("ahmedrachid/FinancialBERT-Sentiment-Analysis", num_labels=3)   #Download
-financialbert_model.save_pretrained("./sentiment_models/financialbert")                                                             #Model is saved for local use
+financialbert_model.save_pretrained("./sentiment_models/financialbert_model")                                                             #Model is saved for local use
 financialbert_tokenizer.save_pretrained("./sentiment_models/financialbert_tokenizer")                                               #Model tokenizer is saved for local use
-    #FinBERT
-finbert_tokenizer = BertTokenizer.from_pretrained("ProsusAI/finbert")                                                               #Download
-finbert_model = BertForSequenceClassification.from_pretrained("ProsusAI/finbert", num_labels=3)                                     #Download
-finbert_model.save_pretrained("./sentiment_models/finbert")                                                                         #Model is saved for local use
-finbert_tokenizer.save_pretrained("./sentiment_models/finbert_tokenizer")                                                           #Model tokeneizer is saved for local use
+
 
---
+++ sentiment_models/finbert/download_code.py
@@ -0,0 +1,9 @@
+from transformers import BertTokenizer, BertForSequenceClassification
+from transformers import pipeline
+
+#Download and save the model and tokeneizer for local use.
+    #FinBERT
+finbert_tokenizer = BertTokenizer.from_pretrained("ProsusAI/finbert")                                                               #Download
+finbert_model = BertForSequenceClassification.from_pretrained("ProsusAI/finbert", num_labels=3)                                     #Download
+finbert_model.save_pretrained("./sentiment_models/finbert_model")                                                                         #Model is saved for local use
+finbert_tokenizer.save_pretrained("./sentiment_models/finbert_tokenizer")                                                           #Model tokeneizer is saved for local use
