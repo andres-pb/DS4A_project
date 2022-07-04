a5ef70e581d6ba7cad92c6f5fd8fb738ec9ff08b
andres-pb <andres-pb10@hotmail.com>
2022-07-04 16:59:21 -0500

    sentiment analysis upgrade

--- app/modules/sentiment_analysis.py
+++ app/modules/sentiment_analysis.py
@@ -75,7 +75,7 @@ class Sentiment_predict():
         df.drop([clean_column_1, clean_column], axis = 1, inplace = True)
         return sent_list
 
-    
+
     def sentiment_financialBERT(self, df: pd.core.frame.DataFrame, string_column: str):
         """
         This function executes sentiment prediction using FinBERT over a text column within a pandas data frame.
@@ -150,19 +150,26 @@ class Sentiment_predict():
                 polygon_news_df = pd.DataFrame.from_dict(polygon_data['results'])
             polygon_news_df.drop(['publisher', 'keywords'], axis = 1, inplace = True)
             polygon_news_df['tickers'] = [' '.join(map(str, l)) for l in polygon_news_df['tickers']]
-            #predict Sentiment
-            if model == "FinBERT":
-                polygon_news_df['title_FinBERT'] = self.sentiment_finBERT(polygon_news_df, 'title')
-                polygon_news_df['description_FinBERT'] = self.sentiment_finBERT(polygon_news_df, 'description')
-            elif model == "FinancialBERT":
-                polygon_news_df['title_FinancialBERT'] = self.sentiment_financialBERT(polygon_news_df, 'title')
-                polygon_news_df['description_FinancialBERT'] = self.sentiment_financialBERT(polygon_news_df, 'description')
-            #Upload df.
-            polygon_news_df = pd.concat([polygon_news_df, polygon_prev], ignore_index = True).drop_duplicates().reset_index(drop = True)
-            polygon_news_df.to_csv("./sentiment_models/polygon_df_01_07_2022.csv", index = False, encoding='utf-8')
-            #Polygon plots Data
-            df = polygon_news_df
-            return df
+            #extract new observations
+            df_new = pd.concat([polygon_prev, polygon_news_df], keys=['t1', 't2']).reset_index()
+            df_new = df_new.drop_duplicates(subset = "id", ignore_index= True)
+            df_new = df_new.loc[df_new['level_0'] == 't2']
+            df_new.drop(['level_0', 'level_1'], axis = 1, inplace =True)
+            df_new = df_new.reset_index(drop = True)            
+            #predict Sentiment on new observations only
+            if df_new.shape[0] > 0:
+                df_new['title_FinBERT'] = self.sentiment_finBERT(df_new, 'title')
+                df_new['description_FinBERT'] = self.sentiment_finBERT(df_new, 'description')
+                df_new['title_FinancialBERT'] = self.sentiment_financialBERT(df_new, 'title')
+                df_new['description_FinancialBERT'] = self.sentiment_financialBERT(df_new, 'description')  
+                #join new with prev
+                polygon_news_df = pd.concat([polygon_prev, df_new], ignore_index=True)
+                polygon_news_df.to_csv("./sentiment_models/polygon_df_01_07_2022.csv", index = False, encoding='utf-8') 
+                df = polygon_news_df[['id', 'title', 'author', 'published_utc', 'article_url', 'tickers', 'amp_url', 'image_url', 'description', 'title_' + model, 'description_' + model]]
+                return df
+            else:
+                df = polygon_prev[['id', 'title', 'author', 'published_utc', 'article_url', 'tickers', 'amp_url', 'image_url', 'description', 'title_' + model, 'description_' + model]]
+                return df
         elif source == "Tweets":
             from ..api import Twitter 
             twt = Twitter()
@@ -184,30 +191,62 @@ class Sentiment_predict():
         else:
             raise Exception("Valid sources are: News or Tweets") 
     
-    def sent_pie(self, source: str, model: str, part = None,  ticker = None, query = None):
+    def sent_pie(self, source: str, model: str, ticker: str, part = None, query = None):
         import plotly.express as px
         if source == "News":
-            polygon_news_df = self.sentiment_df(source, model)
+            df_polygon = self.sentiment_df(source, model)
             colname = part + "_" + model
-            df_poly = polygon_news_df[['id','published_utc', colname]]
-            df_poly['date'] = df_poly['published_utc'].str[0:10]
-            df_poly["total_news_day"] = df_poly.groupby('date')['id'].transform(len)
-            df_poly = df_poly.groupby(['date',colname]).size().reset_index()
-            df_poly.rename({0:'count'}, axis = 1, inplace = True)
-            df_poly['date'] = pd.to_datetime(df_poly['date'], format='%Y-%m-%d')
+            if ticker == "ALL":
+                df_poly = df_polygon[['id','published_utc', colname]]
+                df_poly['date'] = df_poly['published_utc'].str[0:10]
+                df_poly["total_news_day"] = df_poly.groupby('date')['id'].transform(len)
+                df_poly = df_poly.groupby(['date',colname]).size().reset_index()
+                df_poly.rename({0:'count'}, axis = 1, inplace = True)
+                df_poly['date'] = pd.to_datetime(df_poly['date'], format='%Y-%m-%d')
+                plot_title = "All News: " + part + "s by sentiment"
+            else:
+                if df_polygon.loc[df_polygon['tickers'].str.contains(ticker, case = False)].shape[0] > 0:
+                    df_poly = df_polygon.loc[df_polygon['tickers'].str.contains(ticker, case = False)]
+                    df_poly = df_poly[['id','published_utc', colname]]
+                    df_poly['date'] = df_poly['published_utc'].str[0:10]
+                    df_poly["total_news_day"] = df_poly.groupby('date')['id'].transform(len)
+                    df_poly = df_poly.groupby(['date',colname]).size().reset_index()
+                    df_poly.rename({0:'count'}, axis = 1, inplace = True)
+                    df_poly['date'] = pd.to_datetime(df_poly['date'], format='%Y-%m-%d')
+                    plot_title = ticker + " News: " + part + "s by sentiment"
+                else:
+                    print(f"No news was found for the {ticker} ticker in the database. Showing results for ALL.")
+                    df_poly = df_polygon[['id','published_utc', colname]]
+                    df_poly['date'] = df_poly['published_utc'].str[0:10]
+                    df_poly["total_news_day"] = df_poly.groupby('date')['id'].transform(len)
+                    df_poly = df_poly.groupby(['date',colname]).size().reset_index()
+                    df_poly.rename({0:'count'}, axis = 1, inplace = True)
+                    df_poly['date'] = pd.to_datetime(df_poly['date'], format='%Y-%m-%d')
+                    plot_title = "All News: " + part + "s by sentiment"
             df = df_poly
-            plot_title = "News: " + part + "s by sentiment"
-        elif source == "Tweets":
-            df_twt = self.sentiment_df(source, model, tick = ticker, quer = query)
-            colname = "tweet_" + model
-            df_twt["total_news_day"] = df_twt.groupby('date')['id'].transform(len)
-            twt_df = df_twt.groupby(['date', colname]).size().reset_index()
-            twt_df.rename({0:'count'}, axis = 1, inplace = True)
-            twt_df['date'] = pd.to_datetime(twt_df['date'], format='%Y-%m-%d')
-            df = twt_df
-            plot_title = "Tweets by Sentiment"
 
-        fig = px.pie(df, values = 'count', names = colname, template='plotly_dark', title = plot_title)
+        elif source == "Tweets":
+            if query is not None:
+                df_twt = self.sentiment_df(source, model, tick = ticker, quer = query)
+                colname = "tweet_" + model
+                df_twt["total_news_day"] = df_twt.groupby('date')['id'].transform(len)
+                twt_df = df_twt.groupby(['date', colname]).size().reset_index()
+                twt_df.rename({0:'count'}, axis = 1, inplace = True)
+                twt_df['date'] = pd.to_datetime(twt_df['date'], format='%Y-%m-%d')
+                df = twt_df
+                plot_title = ticker + " Tweets by Sentiment"
+            else:   
+                df_twt = self.sentiment_df(source, model, tick = ticker, quer = ticker + " AND crypto")
+                colname = "tweet_" + model
+                df_twt["total_news_day"] = df_twt.groupby('date')['id'].transform(len)
+                twt_df = df_twt.groupby(['date', colname]).size().reset_index()
+                twt_df.rename({0:'count'}, axis = 1, inplace = True)
+                twt_df['date'] = pd.to_datetime(twt_df['date'], format='%Y-%m-%d')
+                df = twt_df
+                plot_title = ticker + " Tweets by Sentiment"
+
+
+        fig = px.pie(df, values = 'count', names = colname, template='plotly_dark', title = "")
         fig.update_layout(
             legend_traceorder="reversed",
             title={
@@ -215,11 +254,78 @@ class Sentiment_predict():
                 'x':0.5,
                 'xanchor': 'center',
                 'yanchor': 'top',
-                'font': dict(
-                #family="Courier New, monospace",
-                size=25)
+                'font': dict(family="Courier New, monospace", size=25)
                 },
             )   
         return fig
 
+
+    def sent_bar(self, source: str, model: str, ticker: str, part = None, query = None):
+        import plotly.express as px
+        if source == "News":
+            df_polygon = self.sentiment_df(source, model)
+            colname = part + "_" + model
+            if ticker == "ALL":
+                df_poly = df_polygon[['id','published_utc', colname]]
+                df_poly['date'] = df_poly['published_utc'].str[0:10]
+                df_poly["total_news_day"] = df_poly.groupby('date')['id'].transform(len)
+                df_poly = df_poly.groupby(['date',colname]).size().reset_index()
+                df_poly.rename({0:'count'}, axis = 1, inplace = True)
+                df_poly['date'] = pd.to_datetime(df_poly['date'], format='%Y-%m-%d')
+                plot_title = "Daily News: " + part + "s by sentiment"
+            else:
+                if df_polygon.loc[df_polygon['tickers'].str.contains(ticker, case = False)].shape[0] > 0:
+                    df_poly = df_polygon.loc[df_polygon['tickers'].str.contains(ticker, case = False)]
+                    df_poly = df_poly[['id','published_utc', colname]]
+                    df_poly['date'] = df_poly['published_utc'].str[0:10]
+                    df_poly["total_news_day"] = df_poly.groupby('date')['id'].transform(len)
+                    df_poly = df_poly.groupby(['date',colname]).size().reset_index()
+                    df_poly.rename({0:'count'}, axis = 1, inplace = True)
+                    df_poly['date'] = pd.to_datetime(df_poly['date'], format='%Y-%m-%d')
+                    plot_title = "Daily " + ticker + " News: " + part + "s by sentiment"
+                else:
+                    print(f"No news was found for the {ticker} ticker in the database. Showing results for ALL.")
+                    df_poly = df_polygon[['id','published_utc', colname]]
+                    df_poly['date'] = df_poly['published_utc'].str[0:10]
+                    df_poly["total_news_day"] = df_poly.groupby('date')['id'].transform(len)
+                    df_poly = df_poly.groupby(['date',colname]).size().reset_index()
+                    df_poly.rename({0:'count'}, axis = 1, inplace = True)
+                    df_poly['date'] = pd.to_datetime(df_poly['date'], format='%Y-%m-%d')
+                    plot_title = "Daily News: " + part + "s by sentiment"
+            df = df_poly
+
+        elif source == "Tweets":
+            if query is not None:
+                df_twt = self.sentiment_df(source, model, tick = ticker, quer = query)
+                colname = "tweet_" + model
+                df_twt["total_news_day"] = df_twt.groupby('date')['id'].transform(len)
+                twt_df = df_twt.groupby(['date', colname]).size().reset_index()
+                twt_df.rename({0:'count'}, axis = 1, inplace = True)
+                twt_df['date'] = pd.to_datetime(twt_df['date'], format='%Y-%m-%d')
+                df = twt_df
+                plot_title = "Daily " + ticker + " Tweets by Sentiment"
+            else:   
+                df_twt = self.sentiment_df(source, model, tick = ticker, quer = ticker + " AND crypto")
+                colname = "tweet_" + model
+                df_twt["total_news_day"] = df_twt.groupby('date')['id'].transform(len)
+                twt_df = df_twt.groupby(['date', colname]).size().reset_index()
+                twt_df.rename({0:'count'}, axis = 1, inplace = True)
+                twt_df['date'] = pd.to_datetime(twt_df['date'], format='%Y-%m-%d')
+                df = twt_df
+                plot_title = "Daily " + ticker + " Tweets by Sentiment"
+
+        fig = px.bar(df, x='date', y='count', color=colname, template='plotly_dark', category_orders= {colname: ["neutral", "negative", "positive"]}, title= "")
+        fig.update_layout(
+            legend_traceorder="reversed",
+            legend_title = '',
+            title={
+                'y':0.95,
+                'x':0.5,
+                'xanchor': 'center',
+                'yanchor': 'top',
+                'font': dict(family="Courier New, monospace", size=25)
+            }
+            )
+        fig.layout.xaxis.dtick = 'D1'
+        return fig
 
