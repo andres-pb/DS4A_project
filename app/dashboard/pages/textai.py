2614dad9a047f00a1aa421fc1cd0ee6fbee8c455
prisparu <ppalacios@sura.com.co>
2022-07-06 21:36:16 -0500

    started textai page

--- app/dashboard/pages/textai.py
+++ app/dashboard/pages/textai.py
@@ -4,11 +4,10 @@ from dash import html, dcc, callback
 from matplotlib.pyplot import plot, text
 import pandas as pd
 import datetime as dt
-from app.dashboard.crypto_plots import plot_model_test, plot_importance, plot_lime
 from dash.dependencies import Input, Output
 from app.modules.models_meta import COINS_SELECTION
-from app.api import yahoo_finance, GoogleTrends
-from app.modules.lstm import get_prediction
+from app.api import yahoo_finance, Twitter
+from app import Sentiment_predict
 
 dash.register_page(__name__)
 
@@ -20,8 +19,18 @@ CONTENT_STYLE = {
     "padding": "2rem 1rem",
 }
 
-# Read predictions obtained during model testing
+# Read tweets and news
+#sp = Sentiment_predict()
+#news_df = sp.sentiment_df(source = "News", model = "FinBERT")
+# tweets_df = sp.sentiment_df(
+#     source = "Tweets", 
+#     model = "FinBERT", 
+#     tick = "ALL", 
+#     quer = "crypto OR blockchain OR cryptocurrency OR coin bitcoin OR BTC OR Ethereum OR ETH OR Litecoin OR LTC"
+# )
 
+
+tweets_df = pd.read_csv('./tweets_all.csv', parse_dates=['created_at'])
 
 layout = html.Div(
     [
@@ -33,10 +42,16 @@ layout = html.Div(
                         children=[
                             html.H3('Cryptocurrency:'),
                             dcc.Dropdown(
-                                id='coin-dropdown',
-                                options=[{'label': c, 'value': c[:3]} for c in COINS_SELECTION],
-                                value='BTC - Bitcoin',
-                                clearable=False,                             
+                                id='coin-dropdown-ta',
+                                options=[
+                                    {'label': 'All', 'value': 'ALL'},
+                                    {'label': 'BTC - Bitcoin', 'value': 'Bitcoin'},
+                                    {'label': 'ETH - Ethereum', 'value': 'Ethereum'},
+                                    {'label': 'LTC - Litecoin', 'value': 'Litecoin'}
+                                ],
+                                value='ALL',
+                                clearable=False,
+                                persistence=False                          
                             )
                         ],
                     ),
@@ -58,30 +73,73 @@ layout = html.Div(
                     width={"size": 4, "offset": 0, 'order': 2},
                 ),
                 dbc.Col(
-                    html.Div(
-                        className='dropdown-fc',
-                        children=[
-                            html.H3('Time Frame:'),
+                    children=[
+                        html.H3('Sentiment:'),
                             dcc.Dropdown(
-                                id='timef-dropdown',
-                                options=['Last 24H', 'Max'],
-                                value='Last 24H',
+                                id='src-dropdown',
+                                options=[{'label': s, 'value': s} for s in ['Positive', 'Negative', 'All']],
+                                value='All',
                                 clearable=False,
                             )
-                        ],
-                    ),
+                    ],
                     width={"size": 4, "offset": 0, 'order': 'last'},
                 )
             ],
             style={'margin-right': '20px','margin-top': '20px', 'margin-bottom': '50px'}
         ),
-
+        dbc.Row([
+            dbc.Col(
+                html.Div(
+                    children=[
+                        html.H3('COUNT OVER TIME'),
+                        dcc.Graph(id='count-bar-plot')
+                        ],
+                    className='graph-container',
+                ),
+                width={"size": 6, "offset": 0, 'order': 2},
+            ),
+        dbc.Col(
+                html.Div(
+                    children=[
+                        html.H3('TOTAL COUNT'),
+                        html.Div(id='total-count')
+                        ],
+                    className='graph-container',
+                ),
+                width={"size": 2, "offset": 0, 'order': 2},
+            ),
+        dbc.Col([
+                html.Div(
+                    [dcc.Loading(
+                        children=[html.Img(
+                                    id='wcloud', 
+                                    src='./assets/wcloud_all.png',
+                                    className='wcloud-image'
+                                    )]
+                        ),],
+                    id='wcloud-container',
+                )],
+                style={'padding': '1rem 1rem 1rem 1rem'},
+                width={"size": 4, "offset": 0, 'order': 2},
+            )
+        ],
+        ),
     ],
     style=CONTENT_STYLE
 )
 
 #============================================CALLBACKS=================================================#
-"""@callback
-(
-    
-)"""
+# @callback(
+#    Output('wcloud', 'src'),
+#    [Input('src-dropdown', 'value'), Input('coin-dropdown-ta', 'value')] 
+# )
+# def plot_words(sel_src, sel_coin):
+
+#     if sel_src == 'Twitter': 
+#         if sel_coin == 'ALL':
+#             wcfig = plot_twt_wordcloud(tweets_df)
+#             dash.get_asset_url(wcfig)
+#         else:
+#             tdf = tweets_df[tweets_df['Ticker']==sel_coin[:3]]
+#             wcfig = plot_twt_wordcloud(tdf)
+#             return dash.get_asset_url(wcfig)
